a24f6a3f9f661286854629acab5c64694850865e
Krishna Yadav <krishnayadav@MacBook-Pro-3.local>
2024-08-28 15:19:20 +0530

    logic

--- summarization_api.py
+++ summarization_api.py
@@ -13,60 +13,32 @@ class Summarization:
         original_input_length = int(length_input * 6)
         print("original length", original_input_length)
         print(length_input)
-        llm = Ollama(model=self.model, temperature=1)          
+        llm = Ollama(model=self.model, temperature=0.3)          
         
-        initial_prompt = f"Generate me a short positive conversation summary of following text: "
+        initial_prompt = f"Generate me a very short positive conversation summary of following text: "
         final_prompt = f"{initial_prompt} '{input_text}'"
         output_positive = llm.invoke(final_prompt)
         print(output_positive)
         
+        if output_positive is None:
+            while output_positive is None:
+                initial_prompt = f"Generate me a very short positive conversation summary of following text: "
+                final_prompt = f"{initial_prompt} '{input_text}'"
+                output_positive = llm.invoke(final_prompt)
+                
         
-        initial_prompt = f"Generate me a short negative conversation summary of following text: "
+        initial_prompt = f"Generate me a very short negative conversation summary of following text: "
         final_prompt = f"{initial_prompt} '{input_text}'"
         output_negative = llm.invoke(final_prompt)
-        print(output_negative)
+        print("\n\n", output_negative)
+             
+        if output_negative is None:
+            while output_negative is None:
+                initial_prompt = f"Generate me a very short negative conversation summary of following text: "
+                final_prompt = f"{initial_prompt} '{input_text}'"
+                output_negative = llm.invoke(final_prompt)
+                
         
-        # else:
-        #     print("Outside 200")
-        #     diff_length = original_input_length - 200
-        #     total_length = int(30 + ( 0.05 *diff_length))
-        #     print(total_length)
-            
-        #     initial_prompt = f"Generate me a positive conversation summary of following text in {total_length} words: "
-        #     # initial_prompt = f"Generate me a positive conversation summary of following text"
-        #     final_prompt = f"{initial_prompt} '{input_text}'"
-        #     output_positive = llm.invoke(final_prompt)
-            
-        #     output_positive_length = len(output_positive.split(' '))
-        #     while True:
-        #         if output_positive_length > total_length:
-        #             print("generating desired words text")
-        #             initial_prompt = f"You generated summary of {output_positive_length} words but I need within {total_length} words"
-        #             print(initial_prompt)
-        #             # initial_prompt = f"Generate me a positive conversation summary of following text"
-        #             final_prompt = f"{initial_prompt} '{input_text}'"
-        #             output_positive = llm.invoke(final_prompt)
-        #             output_positive_length = len(output_positive.split(' '))
-        #             print(output_positive_length)
-                    
-
-        #     initial_prompt = f"Generate me a negative conversation summary of following text in {total_length} words: "
-        #     # initial_prompt = f"Generate me a negative conversation summary of following text"
-        #     final_prompt = f"{initial_prompt} '{input_text}'"
-        #     output_negative = llm.invoke(final_prompt)
-            
-        #     output_negative_length = len(output_negative.split(' '))
-        #     while True:
-        #         if output_negative_length > total_length:
-        #             print("generating desired words text")
-        #             initial_prompt = f"You generated summary of {output_positive_length} words but I need within {total_length} words"
-        #             final_prompt = f"{initial_prompt} '{input_text}'"
-        #             output_negative = llm.invoke(final_prompt)
-        #             output_negative_length = len(output_negative.split(' '))
-        #             print(output_negative_length)
-
-                    
-    
         return output_positive, output_negative
                 
     def summarization(self, input_text):
