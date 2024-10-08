bf4432d54e23bd4ace2339b199dfde1404f797e0
Krishna Yadav <krishnayadav@MacBook-Pro-3.local>
2024-10-08 15:21:49 +0530

    added new db IO mapping

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify
 from summarization_api import Summarization  # Replace with the actual path to your PoemController class
 from langchain_community.llms import Ollama
-
+from store_data import *
 
 app = Flask(__name__)
 
 # Instantiate the PoemController with the required model
-model = "phi3:medium"  # Replace this with the actual model instance
-# model = "phi3"
+# model = "phi3:medium"  # Replace this with the actual model instance
+model = "phi3"
 llm = Ollama(model=model, temperature=0.3)          
 summ_controller = Summarization(llm)
-
+create_database()
 
 @app.route('/summarize', methods=['POST'])
 def summarize_text():
@@ -22,6 +22,12 @@ def summarize_text():
 
     # Call the summarization function
     output_positive, output_negative, output_neutral = summ_controller.summarization(input_text)
+    
+    try:
+        input_string_joined, input_id_joined = list_to_string(input_text)
+        store_data(input_string_joined, input_id_joined, output_positive, output_negative, output_neutral)
+    except Exception as e:
+        print(e)
     
     return jsonify({"summary_positive": output_positive, "summary_negative": output_negative, "summary_neutral": output_neutral})
 
---
+++ store_data.py
@@ -0,0 +1,61 @@
+import json
+import os
+
+def list_to_string(input_text):
+    input_string = []
+    input_id = []
+    
+    for dict1 in input_text:
+        input_string.append(dict1['content'])
+        input_id.append(str(dict1['id']))
+        input_id.append(',')
+    
+    input_string_joined = ' '.join(input_string)
+    input_id_joined = ' '.join(input_id)
+    
+    return input_string_joined, input_id_joined
+
+def create_database():
+    data = {
+        
+    }
+    
+    # Define the file path for saving the JSON file
+    file_path = "data/sentiment_data.json"
+    
+    with open(file_path, 'w') as json_file:
+        json.dump(data, json_file)
+    
+    print(f"Sentiment database created at {file_path}")
+    
+def store_data(input_string_joined, input_id_joined, output_positive, output_negative, output_neutral):
+    file_path = "data/sentiment_data.json"
+    
+    # Check if the file exists, if not create an empty JSON structure
+    if not os.path.exists(file_path):
+        data = {}
+    else:
+        # Open the existing file and load the data
+        with open(file_path, 'r') as json_file:
+            data = json.load(json_file)
+    
+    print(input_id_joined)
+    
+    # Store the input and outputs in the correct format
+    data[input_id_joined.split(',')[0]] = {
+        "input_id" : input_id_joined,
+        "input_text" : input_string_joined,
+        "output_positive": output_positive,
+        "output_negative": output_negative,
+        "output_neutral": output_neutral
+    }
+        
+    # Write the updated data back to the JSON file
+    with open(file_path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)  # Use indent for better readability
+    
+    print(f"New sentiment data saved at {file_path}")
+
+    
+    
+    
